24179fb779205c6d05142fe1f5c925e17e66439b
xamQrexii <usama1630@gmail.com>
2019-06-25 12:46:08 +0500

    Variable hoisting examples

---
+++ variableHoisting.js
@@ -0,0 +1,30 @@
+  
+//VARIABLE HOISTING
+// Try any part one at a time
+
+// var myname;         // Declare Variable
+// myname = 'Osama';   // Initialize variable
+// console.log(myname) // result: Osama
+
+
+
+/*********************************** */
+
+// var myname;          // Declare Variable
+// console.log(myname)  // result: Undefined
+// myname = 'Osama';    // Initialize variable
+
+
+
+/*********************************** */
+
+// console.log(myname)  // result: Undefined
+// myname = 'Osama';    // Initialize variable
+// var myname;          // Declare Variable
+
+
+
+/*********************************** */
+
+// console.log(myname)  // result: ERROR: myname is not defined
+// myname = 'Osama';     // Initialize variable
