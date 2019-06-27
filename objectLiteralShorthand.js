5f38fd18397fb8453037d42fe93dc4048f1be3ae
xamQrexii <usama1630@gmail.com>
2019-06-27 15:36:46 +0500

    Object literal shorthand example

---
+++ objectLiteralShorthand.js
@@ -0,0 +1,26 @@
+//OBJECT LITERAL SHORTHAND
+
+// Try any part one at a time
+
+const firstName = "Muhammad"
+let lastName = "Osama"
+var profession = 'freelancer'
+
+/************************************************** */
+
+// let myObject = {
+//     firstName: firstName,
+//     lastName: lastName,
+//     profession: profession
+// }
+// console.log(myObject)  //RESULT: {firstName: "Muhammad", lastName: "Osama", profession: "freelancer"}
+
+/************************************************** */
+
+// shorthand
+// let myObject = {
+//     firstName,
+//     lastName,
+//     profession
+// }
+// console.log(myObject)  //RESULT: {firstName: "Muhammad", lastName: "Osama", profession: "freelancer"}
